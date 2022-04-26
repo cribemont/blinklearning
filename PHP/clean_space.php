350f222b86033bc14603eee35c737b53ff3b79f8
cribemont <charles.ribemont@gmail.com>
2022-04-26 10:49:11 +0200

    PHP

---
+++ PHP/clean_space.php
@@ -0,0 +1,13 @@
+<?php
+
+$string1 = "Hola que tal soy Charles";
+$string2 = "Hola \nQue tal ?\tSoy Charles ";
+
+// Primer clean, solo espacios
+$string1_clear = str_replace(' ', '', $string1);
+echo $string1_clear;
+
+// Segundo clean con todos los whitespaces, line-breaks, etc, usando preg_replace
+$string2_clear = preg_replace('/\s+/', '', $string2);
+echo $string2_clear;
+?>
